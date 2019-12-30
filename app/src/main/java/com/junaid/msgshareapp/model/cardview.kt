9aa56d68cacff126a81ef2e5e979a72215f92f25
dell <junaid1214@gmail.com>
2019-12-29 23:25:42 -0800

    test

---
+++ app/src/main/java/com/junaid/msgshareapp/model/cardview.kt
@@ -0,0 +1,60 @@
+package com.junaid.msgshareapp.model
+
+ data class cardview(var title:String){
+
+     object supplier {
+         var hobbies= listOf<cardview>(
+
+             cardview("talking"),
+             cardview("sleeping"),
+             cardview("gaming"),
+             cardview("eating"),
+             cardview("walking"),
+             cardview("swimming"),
+             cardview("programing"),
+             cardview("reading"),
+             cardview("writing"),
+             cardview("listening"),
+             cardview("talking"),
+             cardview("sleeping"),
+             cardview("gaming"),
+             cardview("eating"),
+             cardview("walking"),
+             cardview("swimming"),
+             cardview("programing"),
+             cardview("reading"),
+             cardview("writing"),
+             cardview("listening"),
+             cardview("talking"),
+             cardview("sleeping"),
+             cardview("gaming"),
+             cardview("eating"),
+             cardview("walking"),
+             cardview("swimming"),
+             cardview("programing"),
+             cardview("reading"),
+             cardview("writing"),
+             cardview("listening"),
+             cardview("talking"),
+             cardview("sleeping"),
+             cardview("gaming"),
+             cardview("eating"),
+             cardview("walking"),
+             cardview("swimming"),
+             cardview("programing"),
+             cardview("reading"),
+             cardview("writing"),
+             cardview("listening")
+            )
+         }
+     }
+
+
+
+
+
+
+
+
+
+
